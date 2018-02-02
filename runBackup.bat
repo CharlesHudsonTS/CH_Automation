26671bc45e262827767321758ed160084921e520
Shivakumar030 <skumar@chtsinc.com>
2018-02-02 14:16:15 +0530

    runbackup file

---
+++ runBackup.bat
@@ -0,0 +1,18 @@
+set path=C:\\apache-ant-1.9.7\\bin;%path%; 
+set JAVA_HOME=c:\\java8
+
+ant -Dbuild.xml -f Filters TireLineListing contactus  Credut_Card  signin  servicesfinder HomePage TireFinder vehicleselector RecentlyViewed SKUListing SortPagination annoymous browseandshop dynamicvehicle nosearchresult recommendation registration Account_pref Cart TarailerFilter TrailerTireLineListing bookappointment checkout globle test
+ 
+ 
+@ rem sprint 1  targets 
+@ rem test  HomePage  TireFinder  contactus  deals  servicesfinder  signin  vehicleselector
+
+@ rem sprint 2  targets 
+@ rem Filters  RecentlyViewed  SKUListing  SortPagination  TireLineListing  annoymous  browseandshop  dynamicvehicle   
+@ rem nosearchresult  recommendation  registration
+
+@ rem sprint 3 targets 
+@ rem Account_pref  Cart  Credut_Card  TarailerFilter  TrailerTireLineListing  bookappointment  checkout  globle  
+
+
+@rem test means Store Locator
