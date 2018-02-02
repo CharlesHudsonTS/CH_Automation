7cdaa8b7a5798b9d7ec7cdc1703a4649cfd12adc
Shivakumar030 <skumar@chtsinc.com>
2018-02-02 14:14:29 +0530

    bat file

---
+++ run.bat
@@ -0,0 +1,17 @@
+set path=C:\\apache-ant-1.9.7\\bin;%path%; 
+set JAVA_HOME=c:\\java8
+
+ant -Dbuild.xml -f Filters  RecentlyViewed  SKUListing  SortPagination  TireLineListing  annoymous  browseandshop  dynamicvehicle   nosearchresult  recommendation  registration SKUDetails
+
+@ rem sprint 1  targets 
+@ rem test  HomePage  TireFinder  contactus  deals  servicesfinder  signin  vehicleselector
+
+@ rem sprint 2  targets 
+@ rem Filters  RecentlyViewed  SKUListing  SortPagination  TireLineListing  annoymous  browseandshop  dynamicvehicle   
+@ rem nosearchresult  recommendation  registration SKUDetails
+
+@ rem sprint 3 targets 
+@ rem Account_pref  Cart  Credit_Card  TarailerFilter  TrailerTireLineListing  bookappointment  checkout  globle   smoketest
+
+
+@rem test means Store Locator
